a0d86fefec5010a6082bc9d03a121e8c1bd923e6
王宏 <wanghong3@dafycredit.com>
2018-12-11 14:30:31 +0800

    .gitignore

---
+++ hong-redis-service/src/main/java/com/hong/Application_redis.java
@@ -0,0 +1,13 @@
+package com.hong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application_redis
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(Application_redis.class, args);
+    }
+}
